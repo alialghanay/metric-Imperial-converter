f1541753462b895518000c76ac3edc8aad230d3e
ali m alghanay <a.al-ghanay@uot.edu.ly>
2023-04-16 01:45:09 +0200

    Completed Convert Handler file #4 & set tets file to file to fail ...

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 
 // importing packgages
 const express = require('express');
-const routes = require('./routes/routes.js')
+const routes = require('./routes/routes.js');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+
 
 // signing variables
 const app = express();
@@ -14,28 +18,32 @@ const runner            = require('./test-runner');
 // app.use
 app.use("/public", express.static(process.cwd() + "/public"));
 
+app.use(cors({origin: '*'})); //For FCC testing purposes only
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.set
 app.set("views", "./views/pug");
 app.set("view engine", "pug");
 
 //For FCC testing purposes
-fccTestingRoutes(app);
+// fccTestingRoutes(app);
 
 //Routing for API
 routes(app);
 
 app.listen(port, () => {
     console.log(`app listening on Port -> ${port}`)
-    if(process.env.NODE_ENV==='test') {
-        console.log('Running Tests...');
-        setTimeout(function () {
-          try {
-            runner.run();
-          } catch(e) {
-              console.log('Tests are not valid:');
-              console.error(e);
-          }
-        }, 1500);
-      }
-})
+  });
+  // if(process.env.NODE_ENV==='test') {
+  //     console.log('Running Tests...');
+  //     setTimeout(function () {
+  //       try {
+  //         runner.run();
+  //       } catch(e) {
+  //           console.log('Tests are not valid:');
+  //           console.error(e);
+  //       }
+  //     }, 1500);
+  //   }
